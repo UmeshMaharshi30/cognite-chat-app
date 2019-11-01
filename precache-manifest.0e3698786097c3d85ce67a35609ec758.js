e657d6d426708e58eca5d646a4f0a95558d3ce7f
UmeshMaharshi30 <umeshmaharshi@gmail.com>
2019-11-01 13:58:52 -0500

    Updates

---
+++ precache-manifest.0e3698786097c3d85ce67a35609ec758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9063683d0290ac4efa514a386f4b8e3b",
+    "url": "/cognite-chat-app/index.html"
+  },
+  {
+    "revision": "b61319bed679c752362f",
+    "url": "/cognite-chat-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6dc3fbbf83ee56a96ee6",
+    "url": "/cognite-chat-app/static/css/main.d11119a9.chunk.css"
+  },
+  {
+    "revision": "b61319bed679c752362f",
+    "url": "/cognite-chat-app/static/js/2.d7fee1a3.chunk.js"
+  },
+  {
+    "revision": "6dc3fbbf83ee56a96ee6",
+    "url": "/cognite-chat-app/static/js/main.f5fd8184.chunk.js"
+  },
+  {
+    "revision": "4dd6fb1a88ab83622130",
+    "url": "/cognite-chat-app/static/js/runtime-main.de1befff.js"
+  }
+]);
